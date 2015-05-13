baeb84c6cb9d1fc1f42731a1fc9b7a088c1b51e7
Chiunhau <chiunhau9@gmail.com>
2015-05-13 16:02:59 +0800

    wow paperclip is so hard

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,9 @@
 class Card < ActiveRecord::Base
+	has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200" }
+
+	before_save :setImage
+
+	def setImage
+		
+	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150507115407) do
+ActiveRecord::Schema.define(version: 20150513074712) do
 
   create_table "cards", force: :cascade do |t|
     t.text     "url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
